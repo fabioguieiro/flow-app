c13ca23e35c5e47dc953fbc946bedfa00784753a
fabioguieiro <fabioguieiro@gmail.com>
2023-09-27 11:38:11 -0300

    mocking flow chart as showed on test description

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,70 @@
 "use client";
 
-import ReactFlow from "reactflow";
+import { useCallback } from "react";
+import ReactFlow, {
+  addEdge,
+  Background,
+  Connection,
+  Controls,
+  Edge,
+  useEdgesState,
+  useNodesState,
+  BackgroundVariant,
+} from "reactflow";
 
 import "reactflow/dist/style.css";
+
 const initialNodes = [
   { id: "1", position: { x: 0, y: 0 }, data: { label: "START" } },
-  { id: "2", position: { x: 0, y: 100 }, data: { label: "2" } },
-  { id: "3", position: { x: 200, y: 100 }, data: { label: "3" } },
+  { id: "2", position: { x: 0, y: 100 }, data: { label: "Age > 18" } },
+  { id: "3", position: { x: 0, y: 200 }, data: { label: "income > 1000" } },
+  {
+    id: "4",
+    position: { x: 300, y: 100 },
+    data: { label: "decision = FALSE" },
+  },
+  {
+    id: "5",
+    position: { x: 300, y: 200 },
+    data: { label: "decision = FALSE" },
+  },
+  { id: "6", position: { x: 0, y: 300 }, data: { label: "decision = TRUE" } },
 ];
+
 const initialEdges = [
   { id: "e1-2", source: "1", target: "2" },
-  { id: "e2-3", source: "2", target: "3" },
+  { id: "e2-3", source: "2", target: "3", label: "yes" },
+  {
+    id: "e2-4",
+    source: "2",
+    target: "4",
+    label: "no",
+  },
+  { id: "e3-5", source: "3", target: "5", label: "no" },
+  { id: "e3-6", source: "3", target: "6", label: "yes" },
 ];
 
 export default function Home() {
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+  const onConnect = useCallback(
+    (params: Edge<any> | Connection) => setEdges((eds) => addEdge(params, eds)),
+    [setEdges],
+  );
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="flex  h-[32rem] w-[32rem] flex-col items-center justify-center">
-        <ReactFlow nodes={initialNodes} edges={initialEdges} />
+      <div className="flex  h-[32rem] w-[32rem] flex-col items-center justify-center bg-gray-700">
+        <ReactFlow
+          nodes={nodes}
+          edges={edges}
+          onNodesChange={onNodesChange}
+          onEdgesChange={onEdgesChange}
+          onConnect={onConnect}
+        >
+          <Controls />
+          <Background variant={BackgroundVariant.Dots} gap={12} size={1} />
+        </ReactFlow>
       </div>
     </main>
   );
